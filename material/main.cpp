1383674499519f803a63b62aaea757806a5888bf
TorresPepioArnau <torrespepioarnau@gmail.com>
2025-10-05 17:20:57 +0200

    Create main.cpp

---
+++ material/main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+void menu (){
+    int opcio;
+         cout << "********************" << endl;
+    switch(opcio){
+        case 01:
+
+            cout << "* 01: Llegir dades *" << endl;
+            cout << "********************" << endl;
+
+            break;
+
+        case 02:
+
+            break;
+
+        case 03:
+
+            break;
+
+        case 00:
+
+        break;
+
+       default:
+            cout << "*  Opcio invalida  *" << endl;
+            cout << "********************" << endl;
+        break;
+
+        }
+    }
