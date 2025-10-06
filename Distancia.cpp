784461be8db2ad1032cbd831a1ab2acf211b477d
Blast <72162714+BlastES@users.noreply.github.com>
2025-10-06 16:09:57 +0200

    Distancia acabada
    Falta testejar

---
+++ Distancia.cpp
@@ -0,0 +1,60 @@
+#include "material\classes\Distancia.h"
+
+Distancia::Distancia(int codi){
+    this->codi = codi;
+}
+
+void Distancia::afegir(int id, int ordre, string tipusTrajecte, int horaIn, int durada, string comarcaIn, string comarcaFi, string mitjaPrincipal, int edat, string estudis){
+    bool trobat = false;
+    list<Transport>::iterator it = transports.begin();
+    if(!transports.empty()){
+        while(!trobat && it != transports.end()){
+            if(mitjaPrincipal == it->obtenirNom())
+                trobat = true;
+            else
+                it++;
+        }
+    }
+    if(trobat){
+        it->afegir(id, ordre, tipusTrajecte, horaIn, durada, comarcaIn, comarcaFi, edat, estudis);
+    }else{
+        transports.push_back(Transport(mitjaPrincipal));
+        transports.back().afegir(id, ordre, tipusTrajecte, horaIn, durada, comarcaIn, comarcaFi, edat, estudis);
+    }
+}
+
+void Distancia::ordenar_list(){
+    transports.sort(criteri_ordenacio);
+}
+
+int Distancia::nombreTransports() const{
+    return transports.size();
+}
+
+vector<pair<string, int>> Distancia::nombrePersonesPerTransport() const{
+    vector<pair<string, int>> vec;
+    pair<string, int> elem;
+    for(Transport it : transports){
+        elem.first = it.obtenirNom();
+        elem.second = it.obtenirNombrePersones();
+        vec.push_back(elem);
+    }
+    return vec;
+}
+
+pair<string, double> Distancia::mesRapid() const{
+    pair<string,double> mes_rapid;
+    for(Transport it : transports){
+        if(mes_rapid.second > it.obtenirTempsPromig()){
+            mes_rapid.first = it.obtenirNom();
+            mes_rapid.second = it.obtenirTempsPromig();
+        }
+    }
+    return mes_rapid;
+}
+
+bool Distancia::criteri_ordenacio(const Transport &a, const Transport &b) const{
+    if(a.obtenirNombrePersones() > b.obtenirNombrePersones()) return true;
+    else if(a.obtenirNombrePersones() < b.obtenirNombrePersones()) return false;
+    else return a.obtenirNom() > b.obtenirNom();
+}
--- material/classes/Distancia.h
+++ material/classes/Distancia.h
@@ -6,17 +6,26 @@
 #define DISTANCIA_H
 
 #include <string>
+#include <list>
+#include "Transport.h"
 
 using namespace std;
 
 class Distancia {
-    public: 
+    public:
+        Distancia(int codi);
         void afegir(int id, int ordre, string tipusTrajecte, int horaIn, int durada, string comarcaIn, string comarcaFi, string mitjaPrincipal, int edat, string estudis);
+        void ordenar_list();
         int nombreTransports() const;
-        CONTENIDOR nombrePersonesPerTransport() const;
+        vector<pair<string, int>> nombrePersonesPerTransport() const;
         pair<string,double> mesRapid() const;
 
     private:
+        // Atributs
+        int codi;
+        list<Transport> transports;
+
+        bool criteri_ordenacio(const Transport &a, const Transport &b) const;
 };
 
 #endif // DISTANCIA_H
